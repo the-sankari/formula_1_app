2b9e38399e056abefa729523bf448d08600f218c
Kajol sutra dhar <kajol.sd777@gmail.com>
2024-04-30 10:03:26 +0300

    navigation correction done

--- src/Controller/Formula1Controller.php
+++ src/Controller/Formula1Controller.php
@@ -9,7 +9,7 @@ use App\Service\CallApiService;
 
 class Formula1Controller extends AbstractController
 {
-    #[Route('/', name: 'app_formula1')]
+    #[Route('/formula1', name: 'app_formula1')]
     public function index(CallApiService $callApiService): Response
     {
         return $this->render('formula1/index.html.twig', [
